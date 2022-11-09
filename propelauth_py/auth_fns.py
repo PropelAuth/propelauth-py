dfc8f0cd36ebdbe4c70fa8df108e620be2d3d67d
Justin McGuire <jm@landedstar.com>
2022-11-09 10:33:04 -0500

    correcting parameter names

--- propelauth_py/auth_fns.py
+++ propelauth_py/auth_fns.py
@@ -24,30 +24,30 @@ def wrap_validate_access_token_and_get_user_with_org_by_minimum_role(token_verif
 
 
 def wrap_validate_access_token_and_get_user_with_org_by_exact_role(token_verification_metadata):
-    def validate_access_token_and_get_user_with_org_by_exact_role(authorization_header, required_org_id, minimum_required_role=None):
+    def validate_access_token_and_get_user_with_org_by_exact_role(authorization_header, required_org_id, required_role=None):
         access_token = _extract_token_from_authorization_header(authorization_header)
         user = _validate_access_token_and_get_user(access_token, token_verification_metadata)
-        org_member_info = validate_exact_org_role_and_get_org(user, required_org_id, minimum_required_role)
+        org_member_info = validate_exact_org_role_and_get_org(user, required_org_id, required_role)
         return UserAndOrgMemberInfo(user, org_member_info)
 
     return validate_access_token_and_get_user_with_org_by_exact_role
 
 
 def wrap_validate_access_token_and_get_user_with_org_by_permission(token_verification_metadata):
-    def validate_access_token_and_get_user_with_org_by_permission(authorization_header, required_org_id, minimum_required_role=None):
+    def validate_access_token_and_get_user_with_org_by_permission(authorization_header, required_org_id, permission=None):
         access_token = _extract_token_from_authorization_header(authorization_header)
         user = _validate_access_token_and_get_user(access_token, token_verification_metadata)
-        org_member_info = validate_permission_and_get_org(user, required_org_id, minimum_required_role)
+        org_member_info = validate_permission_and_get_org(user, required_org_id, permission)
         return UserAndOrgMemberInfo(user, org_member_info)
 
     return validate_access_token_and_get_user_with_org_by_permission
 
 
 def wrap_validate_access_token_and_get_user_with_org_by_all_permissions(token_verification_metadata):
-    def validate_access_token_and_get_user_with_org_by_all_permissions(authorization_header, required_org_id, minimum_required_role=None):
+    def validate_access_token_and_get_user_with_org_by_all_permissions(authorization_header, required_org_id, permissions=None):
         access_token = _extract_token_from_authorization_header(authorization_header)
         user = _validate_access_token_and_get_user(access_token, token_verification_metadata)
-        org_member_info = validate_all_permissions_and_get_org(user, required_org_id, minimum_required_role)
+        org_member_info = validate_all_permissions_and_get_org(user, required_org_id, permissions)
         return UserAndOrgMemberInfo(user, org_member_info)
 
     return validate_access_token_and_get_user_with_org_by_all_permissions
