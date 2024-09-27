1ca79b0abacf858a921261ed98d2de11579b4d13
Matt Mauer <59808764+mrmauer@users.noreply.github.com>
2024-09-27 11:45:23 -0400

    bump version to 3.1.18

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup(
     name="propelauth-py",
-    version="3.1.17",
+    version="3.1.18",
     description="A python authentication library",
     long_description=README,
     long_description_content_type="text/markdown",
