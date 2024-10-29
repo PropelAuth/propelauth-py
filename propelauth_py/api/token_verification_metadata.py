f248150f9ed596d3ca97db2114aa641041eb611d
pfvatterott <pfvatterott@gmail.com>
2024-10-29 15:13:09 -0600

    Update token_verification_metadata.py

--- propelauth_py/api/token_verification_metadata.py
+++ propelauth_py/api/token_verification_metadata.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from typing import Optional
 import requests
 from propelauth_py.api import _ApiKeyAuth, TokenVerificationMetadata
 
@@ -11,7 +11,7 @@ ENDPOINT_PATH = "/api/v1/token_verification_metadata"
 def _fetch_token_verification_metadata(
     auth_url: str,
     integration_api_key: str,
-    token_verification_metadata: TokenVerificationMetadata | None,
+    token_verification_metadata: Optional[TokenVerificationMetadata],
 ):
     if token_verification_metadata is not None:
         return token_verification_metadata
