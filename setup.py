44af2211f8bc38775b7f3ee7426156f0fa2ebf2c
Andrew Israel <andrew@propelauth.com>
2025-03-12 01:54:49 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup(
     name="propelauth-py",
-    version="4.2.2",
+    version="4.2.3",
     description="A python authentication library",
     long_description=README,
     long_description_content_type="text/markdown",
