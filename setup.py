4107a8f4dca39d6a7420a925b7124e60b199b716
Andrew Israel <andrew@propelauth.com>
2025-04-01 03:44:34 -0700

    Update to UserMetadata missed one area

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup(
     name="propelauth-py",
-    version="4.2.4",
+    version="4.2.5",
     description="A python authentication library",
     long_description=README,
     long_description_content_type="text/markdown",
